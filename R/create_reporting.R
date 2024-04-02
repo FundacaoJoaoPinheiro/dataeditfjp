140c9c90b506c0f407d11b4408f48b9087156669
Erick Amorim <erick.ufpa@gmail.com>
2024-04-02 13:20:46 -0300

    update function creage_reporting

--- R/create_reporting.R
+++ R/create_reporting.R
@@ -1,9 +1,9 @@
 create_reporting <- function(path_data){
 
+  #path_data <- "C:/Users/edcam/Downloads/IMRS_BASE_ESPORTE_2000-2022.xlsx"
   path_report <- "R/report.Rmd"
 
   return(
-    rmarkdown::render(input = path_report, params = list(data = path_data))
-
+    rmarkdown::render(input = path_report,  params = list(data = path_data), output_dir = "../")
     )
 }
