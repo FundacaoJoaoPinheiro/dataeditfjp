c6d9b0700302cf45f3f2ea896d1d4e71206ac0aa
Erick Amorim <erick.ufpa@gmail.com>
2024-04-12 16:53:25 -0300

    updated functions

--- R/hampel_filter.R
+++ R/hampel_filter.R
@@ -2,11 +2,11 @@
 #'
 #' @param variable
 #'
-#' @return
+#' @return resul
 #' @export
 hampel_filter <- function(variable){
-  limite_inf = median(variable, na.rm = T) - 3 * mad(variable, constant = 1, na.rm = T)
-  limite_sup = median(variable, na.rm = T) + 3 * mad(variable, constant = 1, na.rm = T)
+  limite_inf = stats::median(variable, na.rm = T) - 3 * stats::mad(variable, constant = 1, na.rm = T)
+  limite_sup = stats::median(variable, na.rm = T) + 3 * stats::mad(variable, constant = 1, na.rm = T)
   result = (variable < limite_inf | variable > limite_sup)
   return(result)
 }
