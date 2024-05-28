874970ca596747bf81b89a8e62cc09027147a1d1
igortadeu <35731133+igortadeu@users.noreply.github.com>
2024-05-28 15:03:18 -0300

    Arquivo atualizado
    Atualização do arquivo com as funções de criação de gráficos.

--- R/aux_functions.R
+++ R/aux_functions.R
@@ -1,43 +1,3 @@
-#' @title Compare values
-#' @description
-#' Compare the actual value with the before
-#'
-#' @param var
-#'
-#' @return Vector with TRUE or FALSE
-#' @export
-
-compare_lag <- function(var){
-  return(var >= dplyr::lag(var, n = 1))
-}
-
-#' @title Diference in percentual
-#' @description
-#' Calculate the difference between the percentage of municipal participation in the year and the participation in previous year
-#'
-#' @param df
-#' @param var
-#'
-#' @return Data frame containing the difference
-#' @export
-difpercentual <- function(df, var){
-  df |>
-    dplyr::group_by(ANO) |>
-    dplyr::mutate(
-      total = sum({{var}}, na.rm = TRUE)
-    ) |>
-    dplyr::ungroup() |>
-    dplyr::group_by(IBGE7) |>
-    dplyr::summarise(
-      ANO      = ANO,
-      {{var}}  := {{var}},
-      part     = {{var}} / total,
-      part_lag = dplyr::lag(part, n = 1),
-      dif_prop = part - part_lag,
-      .groups  = "drop"
-    )
-}
-
 #' @title Outlier data filter
 #' @description
 #' A short description...
@@ -77,6 +37,7 @@ outlier_detection <- function(var, a = -4, b = 3){
   return(check)
 }
 
+## Comparison between the values as first difference
 compare_first_dif <- function(var){
   dif <- abs(round(c(NA_real_, diff(var)), 2))
   dif_max <- max(dplyr::lag(dif), na.rm = T)
@@ -99,7 +60,7 @@ table_descriptive <- function(var, df = data){
       `D. P.`  = sd(.data[[var]], na.rm = T),
       `C. V.`  = `D. P.` / `Média`,
       Zero     = sum(.data[[var]] == 0, na.rm = T),
-      Missing  = sum(is.na(.data[[var]]))
+      Ausente  = sum(is.na(.data[[var]]))
     ) |>
     dplyr::ungroup() |>
     dplyr::mutate(
@@ -178,8 +139,8 @@ chart_histogram <- function(var, df = data){
   return(result)
 }
 
-# Build bar chart
-chart_bar <- function(ano, n){
+# Build horizontal bar chart
+chart_hbar <- function(ano, n){
   result <-
     data.frame(ano, n) |>
     ggplot2::ggplot(ggplot2::aes(x = as.character(ano), y = as.integer(n))) +
@@ -196,6 +157,64 @@ chart_bar <- function(ano, n){
        legend.position = "none",
        axis.title.x    = ggplot2::element_blank(),
        axis.text.x     = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+    )
+
+  return(print(result))
+}
+
+# Build vertical bar chart
+chart_vbar <- function(df){
+  result <-
+    df |>
+    dplyr::group_by(Regra) |>
+    dplyr::summarise(
+      Validada = round(sum(Validada) / sum(Total) * 100, 2),
+      Suspeita = round(sum(Suspeita) / sum(Total) * 100, 2),
+      Ausente  = round(sum(Ausente) / sum(Total) * 100, 2)
+    ) |>
+    dplyr::mutate(Regra = stringr::str_remove(Regra, "DF_[[:upper:]]{2}_[[:upper:]]+_")) |>
+    tidyr::pivot_longer(cols = !Regra, names_to = "categoria", values_to = "pct") |>
+    ggplot2::ggplot(ggplot2::aes(x = pct, y = Regra, fill = categoria)) +
+    ggplot2::geom_bar(position = "stack", stat = "identity") +
+    ggplot2::scale_fill_brewer(palette = "Set2") +
+    ggplot2::scale_x_continuous(
+      labels = scales::label_percent(scale = 1, big.mark = ".", decimal.mark = ",")
+    ) +
+    ggplot2::labs(
+      title = "Percentual geral de classificação das observações por regra",
+      x     = NULL,
+      y     = "Regra\n",
+      fill  = NULL
+    ) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "top"
+    )
+
+  return(print(result))
+}
+
+# Build heatmap
+chart_heatmap <- function(df){
+  result <-
+    df |>
+    dplyr::mutate(
+      Ano   = as.character(Ano),
+      Regra = stringr::str_remove(Regra, "DF_[[:upper:]]{2}_[[:upper:]]+_")
+    ) |>
+    ggplot2::ggplot(ggplot2::aes(x = Regra, y = Ano, fill = Suspeita)) +
+    ggplot2::geom_tile() +
+    ggplot2::geom_text(ggplot2::aes(label = Suspeita), size = 3) +
+    ggplot2::scale_fill_distiller(palette = "OrRd", direction = 1) +
+    ggplot2::labs(
+      title = "Total de observações suspeitas por regra e ano",
+      x     = "\nRegra",
+      y     = "Ano\n"
+    ) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "None",
+      axis.text.x     = ggplot2::element_text(angle = 45, hjust = 1)
     )
 
   return(print(result))
