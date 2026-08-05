c3d97358b67336194fb7aa9619709a8ef3f32ad1
Erick Amorim <erick.ufpa@gmail.com>
2026-08-05 11:39:32 -0300

    update package

--- R/outlier_function.R
+++ R/outlier_function.R
@@ -1,20 +1,15 @@
 #' @title Detect outlier
 #' @description
-#' Detect outlier as well as dataMaid::identifyOutliers()
-#' 'mc' is the 'medcouple', a robust concept and estimator of skewness, and a and b are appropriate constants (-4 and 3).
-#' The medcouple is defined as a scaled median difference of the left and right half of distribution,
-#' and hence not based on the third moment as the classical skewness.
-#'
+#' Detects outliers in historical data, exluding the last observation.
 #' @param variable quantitative variable
-#' @importFrom robustbase mc
-#' @import robustbase
+#' @importFrom dplyr lag
+#' @import dplyr
 #' @return Vector with TRUE or FALSE
 
 outlier_function <- function(variable){
-  q1 <- stats::quantile(variable, probs = 0.25, na.rm = T, names = F)
-  q3 <- stats::quantile(variable, probs = 0.75, na.rm = T, names = F)
-  iqr <- stats::IQR(variable, na.rm = T)
-  #mc <- robustbase::mc(variable, na.rm = T)
+  q1 <- stats::quantile(dplyr::lag(variable), probs = 0.25, na.rm = T, names = F)
+  q3 <- stats::quantile(dplyr::lag(variable), probs = 0.75, na.rm = T, names = F)
+  iqr <- stats::IQR(dplyr::lag(variable), na.rm = T)
   inf <- q1 - 2*iqr
   sup <- q3 + 2*iqr
 
