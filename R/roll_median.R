e4bdf73dcf297c9645e92c27f4829e73cc7308b7
Erick Amorim <erick.ufpa@gmail.com>
2026-08-06 07:43:30 -0300

    update report and function

--- R/roll_median.R
+++ R/roll_median.R
@@ -4,14 +4,11 @@
 #'
 #' @param variable A variable representing a series of observations.
 #' @param fator A scale parameter.
-#' @param defaut If false return the values with TRUE or FALSE if the observation is above 20% (fator) the  roll median of the last 3 years.
 #' @import zoo
 #' @importFrom zoo rollmedian
 #' @return Vector with TRUE or FALSE if the observation is below 20%(fator) of the roll median of the last 3 years
 
-roll_median <- function(variable, fator, defaut = F){
-  if(defaut){res <- dplyr::if_else(variable > (fator * zoo::rollmedian(variable, k = 3, fill = NA, align = "right")), T, F)}
-
-  if(defaut==F){res <- dplyr::if_else(variable < (fator * zoo::rollmedian(variable, k = 3, fill = NA, align = "right")), T, F)}
+roll_median <- function(variable, fator){
+  res <- fator*zoo::rollmedian(variable, k = 3, fill = NA, align = "right")
   return(res)
 }
